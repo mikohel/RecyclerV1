2d2848b8bb976d211650408ad5ce7f9139be5657
mikohel <mikohel96@gmail.com>
2022-05-25 07:58:17 -0500

    icon

--- app/src/main/java/com/example/recyclerv1/MainActivity.kt
+++ app/src/main/java/com/example/recyclerv1/MainActivity.kt
@@ -14,11 +14,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val fakeVideoData: Array<JSONObject> = arrayOf(
-            JSONObject("{\"title\": \"An awesome video title\", \"channel\": \"ChannelName\", \"views\": \"6k views\", \"datePosted\": \"5 days ago\", \"duration\": \"05:00\"}"),
-            JSONObject("{\"title\": \"John\", \"channel\": \"Channel\", \"views\": \"8k views\", \"datePosted\": \"5 days ago\", \"duration\": \"05:00\"}"),
-            JSONObject("{\"title\": \"John\", \"channel\": \"Channel\", \"views\": \"10k views\", \"datePosted\": \"5 days ago\", \"duration\": \"05:25\"}"),
-            JSONObject("{\"title\": \"John\", \"channel\": \"Channel\", \"views\": \"150k views\", \"datePosted\": \"A month ago\", \"duration\": \"05:02\"}"),
-            JSONObject("{\"title\": \"John\", \"channel\": \"Channel\", \"views\": \"254k views\", \"datePosted\": \"2 years ago\", \"duration\": \"05:00\"}")
+            JSONObject("{\"title\": \"Auron play - Amongos\", \"channel\": \"AuronPlay\", \"views\": \"600k views\", \"datePosted\": \"5 days ago\", \"duration\": \"15:00min\"}"),
+            JSONObject("{\"title\": \"Dross- Canitas\", \"channel\": \"DROSS\", \"views\": \"800k views\", \"datePosted\": \"1 month ago\", \"duration\": \"12:00min\"}"),
+            JSONObject("{\"title\": \"Gucci - el chgico gucci\", \"channel\": \"Gucci\", \"views\": \"10k views\", \"datePosted\": \"21 days ago\", \"duration\": \"05:25\"}"),
+            JSONObject("{\"title\": \"La ruta de la Garnacha - COME SANO\", \"channel\": \"La Ruta Garnachera\", \"views\": \"150k views\", \"datePosted\": \"A month ago\", \"duration\": \"05:02\"}"),
+            JSONObject("{\"title\": \"Mikohel -  tenog tu love cover\", \"channel\": \"Mikohel\", \"views\": \"254k views\", \"datePosted\": \"2 years ago\", \"duration\": \"05:00\"}")
 
         )
 
--- app/src/main/java/com/example/recyclerv1/MainAdapter.kt
+++ app/src/main/java/com/example/recyclerv1/MainAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.recyclerv1
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -23,13 +24,15 @@ class MainAdapter(private val videos: Array<JSONObject>): RecyclerView.Adapter<M
     class MainHolder(val binding: ItemVideoBinding): RecyclerView.ViewHolder(binding.root){
         fun render(video: JSONObject){
 
-
+            binding.ivVideo.setImageResource(R.drawable.ic_auron2_background)
             binding.tvDescription.setText(video.getString("title"))
             binding.tvChannelName.setText(video.getString("channel"))
             binding.tvViews.setText(video.getString("views"))
             binding.tvDate.setText(video.getString("datePosted"))
             binding.tvDuration.setText(video.getString("duration"))
+
         }
+
 
     }
 }
